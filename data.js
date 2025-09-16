45035001964fc9731e91812886778f5be21d0737
waeson <59735306+waeson@users.noreply.github.com>
2025-09-17 02:21:20 +0700

    СОНГКМ загрузил

---
+++ data.js
@@ -0,0 +1,781 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0---",
+      "name": "СОНГКМ ВЖК обзорная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.463845894085676,
+        "pitch": 0.6559707326369626,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.354197504296307,
+          "pitch": 0.3216721653496819,
+          "rotation": 3.141592653589793,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--",
+      "name": "СОНГКМ вход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.991295749910888,
+        "pitch": 0.21609903287899002,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9111166639129742,
+          "pitch": -0.06188741435083145,
+          "rotation": 0,
+          "target": "0---"
+        },
+        {
+          "yaw": -2.1921195524905475,
+          "pitch": 0.228269884043117,
+          "rotation": 3.141592653589793,
+          "target": "6---"
+        },
+        {
+          "yaw": 2.4685308444411245,
+          "pitch": 0.022324956514534477,
+          "rotation": 3.141592653589793,
+          "target": "2---"
+        },
+        {
+          "yaw": -2.2194676565864793,
+          "pitch": 0.00729337913663386,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---",
+      "name": "СОНГКМ коридор центр",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.5691647299445783,
+        "pitch": 0.1485910476430483,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.518249951669766,
+          "pitch": -0.056570620421400264,
+          "rotation": 5.497787143782138,
+          "target": "4--2-"
+        },
+        {
+          "yaw": -2.6841079464976403,
+          "pitch": 0.07143437852851164,
+          "rotation": 3.141592653589793,
+          "target": "1--"
+        },
+        {
+          "yaw": 0.40263639841243304,
+          "pitch": -0.06969218344358197,
+          "rotation": 3.141592653589793,
+          "target": "3--"
+        },
+        {
+          "yaw": -1.113077041253172,
+          "pitch": -0.009784567276916789,
+          "rotation": 1.5707963267948966,
+          "target": "12--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "СОНГКМ коридор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.238974984535348,
+          "pitch": -0.045500290556423906,
+          "rotation": 3.141592653589793,
+          "target": "6---"
+        },
+        {
+          "yaw": 2.2607926534695304,
+          "pitch": 0.19539768580298222,
+          "rotation": 1.5707963267948966,
+          "target": "5---"
+        },
+        {
+          "yaw": -0.2650812605911401,
+          "pitch": -0.09198886190825561,
+          "rotation": 1.5707963267948966,
+          "target": "14--2"
+        },
+        {
+          "yaw": -2.0005626111008485,
+          "pitch": 0.03351704677300482,
+          "rotation": 3.141592653589793,
+          "target": "2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--2-",
+      "name": "СОНГКМ 2 этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7858389077717227,
+          "pitch": 0.010191326741118445,
+          "rotation": 3.141592653589793,
+          "target": "11---"
+        },
+        {
+          "yaw": 2.459762975977897,
+          "pitch": 0.2529892602584578,
+          "rotation": 3.9269908169872414,
+          "target": "2---"
+        },
+        {
+          "yaw": 0.6356686285771911,
+          "pitch": -0.037474247317714315,
+          "rotation": 4.71238898038469,
+          "target": "13--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5---",
+      "name": "СОНГКМ компьютерный кабинет",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -3.021528379703927,
+        "pitch": 0.3060731441141833,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0251747693211026,
+          "pitch": -0.08019274492801998,
+          "rotation": 4.71238898038469,
+          "target": "3--"
+        },
+        {
+          "yaw": 2.0302539511260296,
+          "pitch": 0.12802915759227318,
+          "rotation": 1.5707963267948966,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6---",
+      "name": "СОНГКМ ВЖК коридор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.988507115077854,
+        "pitch": 0.20043550163908463,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6568397779878801,
+          "pitch": 0.019396749750736575,
+          "rotation": 3.141592653589793,
+          "target": "8---"
+        },
+        {
+          "yaw": -2.5022112587717853,
+          "pitch": 0.1289647501677056,
+          "rotation": 3.141592653589793,
+          "target": "7---"
+        },
+        {
+          "yaw": 2.5306988342102965,
+          "pitch": -0.014732505463662449,
+          "rotation": 3.141592653589793,
+          "target": "10--"
+        },
+        {
+          "yaw": 0.03262253272104232,
+          "pitch": 0.12237967788630044,
+          "rotation": 3.141592653589793,
+          "target": "1--"
+        },
+        {
+          "yaw": 0.026263017788949128,
+          "pitch": -0.09037986613721927,
+          "rotation": 0,
+          "target": "0---"
+        },
+        {
+          "yaw": -3.1007275056150014,
+          "pitch": 0.04518346161037279,
+          "rotation": 4.71238898038469,
+          "target": "9---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7---",
+      "name": "СОНГКМ ВЖК подсобка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.7651708376293058,
+        "pitch": 0.44088331423968086,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3585393910223065,
+          "pitch": 0.34915027183713576,
+          "rotation": 4.71238898038469,
+          "target": "6---"
+        },
+        {
+          "yaw": -1.3654573082832098,
+          "pitch": -0.1139474406322627,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8---",
+      "name": "СОНГКМ ВЖК прачечная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.65172224377239,
+        "pitch": 0.5955909245205468,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3899652724464069,
+          "pitch": 0.24078981446731618,
+          "rotation": 1.5707963267948966,
+          "target": "6---"
+        },
+        {
+          "yaw": 0.3944118193185844,
+          "pitch": -0.01697375978026905,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9---",
+      "name": "СОНГКМ ВЖК санузел",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 3.083475400670454,
+        "pitch": 0.2876413817424428,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5034364669982967,
+          "pitch": 0.1724504677717995,
+          "rotation": 1.5707963267948966,
+          "target": "6---"
+        },
+        {
+          "yaw": 1.512928428848407,
+          "pitch": -0.028536892546757286,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10--",
+      "name": "СОНГКМ гостевая",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.3572924472851504,
+        "pitch": 0.4425337912104723,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0546859142191884,
+          "pitch": 0.2293942716056634,
+          "rotation": 3.141592653589793,
+          "target": "6---"
+        },
+        {
+          "yaw": 1.060441188314833,
+          "pitch": -0.09082144365320843,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11---",
+      "name": "СОНГКМ зимний сад",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.5548156138334246,
+        "pitch": 0.40419748008242706,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.538435559976195,
+          "pitch": 0.1900358487244045,
+          "rotation": 3.141592653589793,
+          "target": "4--2-"
+        },
+        {
+          "yaw": 0.5853483646980013,
+          "pitch": -0.0767943476392361,
+          "rotation": 3.9269908169872414,
+          "target": "2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12--",
+      "name": "СОНГКМ прачечная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.4567623959710545,
+        "pitch": 0.5388943690501371,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0062137090150074,
+          "pitch": 0.19968977489271822,
+          "rotation": 4.71238898038469,
+          "target": "2---"
+        },
+        {
+          "yaw": -2.000169990687871,
+          "pitch": -0.042880691819275896,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13--2",
+      "name": "СОНГКМ спортзал2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.009078224528280998,
+        "pitch": 0.5288776544736855,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.19442921079341,
+          "pitch": 0.05347661770293399,
+          "rotation": 3.141592653589793,
+          "target": "4--2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14--2",
+      "name": "СОНГКМ столовая2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.1077909710747882,
+        "pitch": 0.26283674416108127,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.639402242609158,
+          "pitch": 0.07485152889239899,
+          "rotation": 1.5707963267948966,
+          "target": "3--"
+        },
+        {
+          "yaw": 0.33485428555420604,
+          "pitch": -0.024577011283879457,
+          "rotation": 0.7853981633974483,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "СОНГКМ внутри",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
